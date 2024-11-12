1dc162cf3a538657dd4542a18bbc36cd01ee0d87
Rebecca <rebeccaj0610@gmail.com>
2024-11-12 00:08:38 -0800

    Initial Commit

---
+++ OwnPost/OwnPostApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnPostApp.swift
+//  OwnPost
+//
+//  Created by Rebecca Joseph on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct OwnPostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
